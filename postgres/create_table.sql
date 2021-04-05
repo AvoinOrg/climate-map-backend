eed1fcabc6758f762303e2dceaa22f749ca2c9be
xbexbex <petja.valkama@gmail.com>
2021-04-05 13:58:27 +0300

    Update db

--- postgres/create_table.sql
+++ postgres/create_table.sql
@@ -8,17 +8,22 @@ CREATE TABLE user_account (
     phone_number varchar,
     funnel_state INT DEFAULT 1,
     account_type varchar DEFAULT 'explorer',
+    email_verified INT default 0,
     UNIQUE(email)
 );
 
-CREATE TABLE user_integrations (
+CREATE TABLE user_integration (
     id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
     user_account_id uuid NOT NULL,
-    vipu_state INT DEFAULT -1,
-    metsaan_state INT DEFAULT -1,
+    integration_type varchar NOT NULL,
+    integration_status INT DEFAULT -1,
+    integration_data jsonb DEFAULT '{}',
+    is_disabled boolean DEFAULT FALSE,
+    UNIQUE(user_account_id, integration_type),
     CONSTRAINT fk_user_account
       FOREIGN KEY(user_account_id) 
 	    REFERENCES user_account(id)
 );
+
 
 CREATE UNIQUE INDEX idx_email ON user_account (email);
