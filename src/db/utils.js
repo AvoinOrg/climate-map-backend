a696a881e15f395ec69c6ff89011debff2d20edb
xbexbex <petja.valkama@gmail.com>
2021-04-05 14:00:26 +0300

    Update utils

--- src/db/utils.js
+++ src/db/utils.js
@@ -1,26 +1,73 @@
 module.exports = {
-    parseRow: (rows) => {
-        if (rows[0]) {
-            return rows[0]
+    parseRows: (rows, single = true, index = null) => {
+        if (!single) {
+            vals = index ? {} : []
+
+            if (!rows || rows.length === 0) {
+                return vals
+            }
+
+            for (row in rows) {
+                if (index) {
+                    indexVal = row[index]
+                    delete row[index]
+                    vals[indexVal] = row
+                } else {
+                    vals.append(row)
+                }
+            }
+
+            return vals
+        } else {
+            if (rows && rows[0]) {
+                return rows[0]
+            }
         }
         return null
     },
 
-    valuesToUpdateString: (values, allowed) => {
-        let s = ''
-        let i = 2
+    valuesToUpdateString: (values, allowed, colsBefore = 1) => {
+        let vars = ''
+        let i = colsBefore + 1
         const vals = []
 
         for (v in values) {
             if (allowed.includes(v)) {
-                s += `${v} = $${i}, `
+                vars += `${v} = $${i}, `
                 vals.push(values[v])
                 i++
             }
         }
 
-        s = s.substr(0, s.length - 2)
-        q = { string: s, values: vals }
+        vars = vars.substr(0, vars.length - 2)
+        q = { vars, vals }
+
+        return q
+    },
+
+    valuesToInsertString: (values, allowed, colsBefore = 1) => {
+        let cols = ''
+        let vars = ''
+        let i = colsBefore + 1
+        const vals = []
+
+        if (colsBefore >= 1) {
+            vars += ", "
+            cols += ", "
+        }
+
+        for (v in values) {
+            if (allowed.includes(v)) {
+                cols += `${v}, `
+                vars += `$${i}, `
+                vals.push(values[v])
+                i++
+            }
+        }
+
+        cols = cols.substr(0, cols.length - 2)
+        vars = vars.substr(0, vars.length - 2)
+        q = { cols, vars, vals }
 
         return q
     },
