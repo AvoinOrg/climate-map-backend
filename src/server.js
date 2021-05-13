2460f03d73c174ac19d2fba25f25d1919a54ac47
xbexbex <petja.valkama@gmail.com>
2021-05-13 12:19:25 +0300

    Remove bodyparser

--- src/server.js
+++ src/server.js
@@ -1,16 +1,15 @@
 const express = require('express')
 const passport = require('passport')
-const bodyParser = require('body-parser')
 const cors = require('cors')
 
-require('./auth/auth')
+require('./utils/auth')
 const routes = require('./routes/routes')
 const secureRoutes = require('./routes/secure-routes')
 
 const app = express().use("*", cors())
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 
 // app.use((req, res, next) => {
 //     res.header('Access-Control-Allow-Origin', '*')
